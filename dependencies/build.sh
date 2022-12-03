acbab5b94859d890c8160d16869958c65d715036
klsruan <rdkleriston@gmail.com>
2022-12-03 18:16:51 -0300

    Fix lodepng load

--- dependencies/build.sh
+++ dependencies/build.sh
@@ -19,6 +19,7 @@ git checkout 784ff113071f1fa7832ebe74667f2fd0756c634f
 
 git clone https://github.com/lvandeve/lodepng.git $sub/lodepng && cd $sub/lodepng
 git checkout 997936fd2b45842031e4180d73d7880e381cf33f
+cp -f $sub/lodepng/lodepng.cpp $sub/lodepng/lodepng.c
 
 git clone https://github.com/rcancro/giflib.git $sub/giflib && cd $sub/giflib
 git checkout 4b0c893cfddf16421bd3f59207fdf65f06e9a10d
